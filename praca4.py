91dd0e3e0b02181b404586c481a5a2924163b9b6
pawson123xd <115405815+pawson123xd@users.noreply.github.com>
2023-04-21 12:16:19 +0200

    Create praca4.py

---
+++ praca4.py
@@ -0,0 +1,20 @@
+def zada6():
+    i='la la la la'
+    print(i.count('la'))
+zada6()
+def zada10():
+    lista=['star wars','batman','supermen','asterix i obelix']
+    lista.sort()
+    print(lista)
+zada10()
+def zada11():
+
+def zada16():
+	lista={'złoto':'gothic','bogowie':'god of war','bloki':'minecraft'}
+	liczba1=0
+	liczba2=0
+	while liczba1<len(lista.keys()):
+    		liczba1+=1
+	while liczba2<len(lista.values()):
+    		liczba2+=1
+	print("{}".format(liczba1+liczba2))
