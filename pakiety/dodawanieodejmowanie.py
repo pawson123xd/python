2a3c1d34e748c2c25c3ba1d7c58117b2804e2362
pawson123xd <115405815+pawson123xd@users.noreply.github.com>
2024-09-21 22:25:29 +0200

    Add files via upload

---
+++ pakiety/dodawanieodejmowanie.py
@@ -0,0 +1,8 @@
+def dodawaanie(a,b,c,d):
+    z=complex(a,b)
+    x=complex(c,d)
+    print('{}'.format(z+x))
+def odjemowanie(a,b,c,d):
+    z=complex(a,b)
+    x=complex(c,d)
+    print('{}'.format(z-x))
---
+++ pakiety/liczbyzespolone.py
@@ -0,0 +1,4 @@
+def liczby(a):
+    print(a)
+def liczba_urojona(a):
+    print('{}'.format(complex(0,a)))
