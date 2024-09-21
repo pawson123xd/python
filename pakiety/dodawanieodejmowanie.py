39d4abc1d069b38e0c0142433d2271216b0333f0
pawson123xd <115405815+pawson123xd@users.noreply.github.com>
2024-09-21 22:26:30 +0200

    Delete pakiety/dodawanieodejmowanie.py

--- pakiety/dodawanieodejmowanie.py
+++
@@ -1,8 +0,0 @@
-def dodawaanie(a,b,c,d):
-    z=complex(a,b)
-    x=complex(c,d)
-    print('{}'.format(z+x))
-def odjemowanie(a,b,c,d):
-    z=complex(a,b)
-    x=complex(c,d)
-    print('{}'.format(z-x))
